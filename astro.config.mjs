9aacdf654006588e421449a168902fa4a8a5c3c8
arthurshem86 <arthurshem86@gmail.com>
2026-04-26 03:00:37 +0300

    update CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://arthurshem86.github.io/astroboards/',
+    site: 'https://arthurshem86.github.io',
+    base: '/astroboards',
 });
 
 
